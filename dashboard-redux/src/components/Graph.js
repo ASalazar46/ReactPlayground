d600cede2062350ef920a488e1baa90a6348ff7d
ASalazar46 <andysal737@gmail.com>
2021-09-23 15:38:59 -0700

    Added in an actual graph

--- dashboard-redux/src/components/Graph.js
+++ dashboard-redux/src/components/Graph.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Line } from 'react-chartjs-2';
 
 export class Graph extends React.Component {
     render() {
+        const data = {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [
+              {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                fill: false,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgba(255, 99, 132, 0.2)',
+              },
+            ],
+          };
+          const options = {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+        };
         return(
             <div>
-                <h1>Graph goes here</h1>
+              <Line data={data} options={options} />
             </div>
         );
     };
