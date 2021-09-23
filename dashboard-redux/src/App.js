49a849a88ac08685d27faaec0d23017e50769638
ASalazar46 <andysal737@gmail.com>
2021-09-23 15:25:36 -0700

    Removed app.css and redid app.js

--- dashboard-redux/src/App.js
+++ dashboard-redux/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import { PageHeader } from './components/PageHeader';
 import { DataAverager } from './components/DataAverager';
 import { Graph } from './components/Graph';
@@ -8,21 +8,27 @@ import { Sidebar } from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      <div class="grid grid-flow-row grid-flow-col grid-rows-10 grid-cols-10 gap-2">
-        <div class="row-start-1 col-start-1 row-end-10 col-end-2">
+      <div className="grid grid-rows-10 grid-cols-12">
+        <div className="row-start-1 col-start-1 row-end-11 col-end-3">
           <Sidebar />
         </div>
-        <div class="row-start-1 col-start-3 row-end-1 col-end-10">
-          <PageHeader />
-        </div>
-        <div class="row-start-2 col-start-3 row-end-10 col-end-5">
-          <DataAverager />
-        </div>
-        <div class="row-start-2 col-start-6 row-end-6 col-end-10">
-          <Graph />
-        </div>
-        <div class="row-start-7 col-start-6 row-end-10 col-end-10">
-          <Map />
+        <div className="row-start-1 col-start-3 row-end-11 col-end-13">
+          <div className="row-start-1 col-start-1 row-end-2 col-end-11">
+            <PageHeader />
+          </div>
+          <div className="row-start-2 col-start-1 row-end-11 col-end-11">
+            <div className="row-start-1 col-start-1 row-end-10 col-end-4">
+              <DataAverager />
+            </div>
+            <div className="row-start-1 col-start-4 row-end-10 col-end-11">
+              <div className="row-start-1 col-start-1 row-end-6 col-end-8">
+                <Graph />
+              </div>
+              <div className="row-start-6 col-start-1 row-end-10 col-end-8">
+                <Map />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -30,3 +36,24 @@ function App() {
 }
 
 export default App;
+/* 
+    <div className="App">
+      <div className="grid grid-rows-10 grid-cols-10">
+        <div className="row-start-1 col-start-1 row-end-10 col-end-2">
+          <Sidebar />
+        </div>
+        <div className="row-start-1 col-start-3 row-end-1 col-end-10">
+          <PageHeader />
+        </div>
+        <div className="row-start-2 col-start-3 row-end-10 col-end-5">
+          <DataAverager />
+        </div>
+        <div className="row-start-2 col-start-6 row-end-6 col-end-10">
+          <Graph />
+        </div>
+        <div className="row-start-7 col-start-6 row-end-10 col-end-10">
+          <Map />
+        </div>
+      </div>
+    </div>
+*/
