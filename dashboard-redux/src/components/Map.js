ac8ae804f6185fe27ea22c4a698da46d685464ba
ASalazar46 <andysal737@gmail.com>
2021-09-23 01:32:43 -0700

    Added the map component

--- dashboard-redux/src/components/Map.js
+++ dashboard-redux/src/components/Map.js
@@ -1,11 +1,22 @@
 import React from "react";
+import '../../node_modules/leaflet/dist/leaflet.css';
+import './map.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export class Map extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Map goes here</h1>
-            </div>
+            <MapContainer id='mapid' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         );
     };
 }
