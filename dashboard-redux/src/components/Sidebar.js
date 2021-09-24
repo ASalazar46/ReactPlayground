37fd336bbc6cf6cd88dee501bc6b733421b648c7
ASalazar46 <andysal737@gmail.com>
2021-09-23 23:31:08 -0700

    Simplified the code to have less div's

--- dashboard-redux/src/components/Sidebar.js
+++ dashboard-redux/src/components/Sidebar.js
@@ -4,24 +4,12 @@ export class Sidebar extends React.Component {
     render() {
         return (
             <div className="grid grid-col-1 grid-row-6">
-                <div className="row-start-1 text-center">
-                    <button>SCEL LOGO</button>
-                </div>
-                <div className="row-start-2 text-center">
-                    <button>Home</button>
-                </div>
-                <div className="row-start-3 text-center"> 
-                    <button>Apple</button>
-                </div>
-                <div className="row-start-4 text-center">
-                    <button>Bumblebee</button>
-                </div>
-                <div className="row-start-5 text-center">
-                    <button>Cranberry</button>
-                </div>
-                <div className="row-start-6 text-center">
-                    <button>Guava</button>
-                </div>
+                <div className="box-border border-4 row-start-1 text-center">SCEL LOGO</div>
+                <button className="box-border border-4 row-start-2 text-center">Home</button>
+                <button className="box-border border-4 row-start-3 text-center">Apple</button>
+                <button className="box-border border-4 row-start-4 text-center">Bumblebee</button>
+                <button className="box-border border-4 row-start-5 text-center">Cranberry</button>
+                <button className="box-border border-4  row-start-6 text-center">Guava</button>
             </div>
         );
     };
