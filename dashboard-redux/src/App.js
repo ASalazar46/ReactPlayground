a33e2d4b94dfc10ec312cbbe30bf99f8ac42f5a9
ASalazar46 <andysal737@gmail.com>
2021-09-24 01:15:42 -0700

    Margin adjustments

--- dashboard-redux/src/App.js
+++ dashboard-redux/src/App.js
@@ -9,22 +9,22 @@ function App() {
   return (
     <div className="App">
       <div className="grid grid-rows-10 grid-cols-12">
-        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2 mr-4">
+        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2 mr-2">
           <Sidebar />
         </div>
         <div className="grid row-start-1 col-start-2 row-end-11 col-end-13">
-          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center mb-4 mr-4">
+          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center mb-2 mr-2">
             <PageHeader />
           </div>
-          <div className="grid row-start-2 col-start-1 row-end-11 col-end-11">
-            <div className="grid row-start-1 col-start-1 row-end-10 col-end-6 mr-4">
+          <div className="grid row-start-2 col-start-1 row-end-11 col-end-11 ml-2">
+            <div className="grid row-start-1 col-start-1 row-end-10 col-end-6 mr-2">
               <DataAverager />
             </div>
-            <div className="grid row-start-1 col-start-6 row-end-10 col-end-11">
-              <div className="grid row-start-1 col-start-1 row-end-6 col-end-8 mr-4">
+            <div className="grid row-start-1 col-start-6 row-end-10 col-end-11 mr-4">
+              <div className="grid row-start-1 col-start-1 row-end-6 col-end-8">
                 <Graph />
               </div>
-              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8 mt-4 mr-4">
+              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8 mt-2">
                 <Map />
               </div>
             </div>
--- dashboard-redux/src/components/DataAverager.js
+++ dashboard-redux/src/components/DataAverager.js
@@ -3,39 +3,39 @@ import React from "react";
 export class DataAverager extends React.Component {
     render() {
         return(
-            <div className="grid grid-rows-2 grid cols-2">
+            <div className="grid grid-rows-2 grid cols-2 space-x-1">
                 <div className="grid row-start-1 col-start-1 row-end-5 col-end-2">
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mb-0.5">
                         Avg. SI
                         324.6 W/M^2
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5 mb-0.5">
                         Avg. Temp
                         392.2 C
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5 mb-0.5">
                         Avg. Pres
                         1018.316 MB
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5">
                         Avg. Hmdy
                         65532
                     </div>
                 </div>
                 <div className="grid row-start-1 col-start-2 row-end-5 col-end-3">
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mb-0.5">
                         Curr. SI
                         324.6 W/M^2
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5 mb-0.5">
                         Curr. Temp
                         392.2 C
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5 mb-0.5">
                         Curr. Pres
                         1018.316 MB
                     </div>
-                    <div class="box-border h-40 w-40 p-4 border-4 m-0">
+                    <div class="box-border h-40 w-40 p-4 border-4 mt-0.5">
                         Curr. Hmdy
                         65532
                     </div>
