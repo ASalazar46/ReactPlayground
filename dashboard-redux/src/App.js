33ebd148200df4d5f50c40c205b07cf0b741a97e
ASalazar46 <andysal737@gmail.com>
2021-09-23 21:17:04 -0700

    Adjusted margins

--- dashboard-redux/src/App.js
+++ dashboard-redux/src/App.js
@@ -9,22 +9,22 @@ function App() {
   return (
     <div className="App">
       <div className="grid grid-rows-10 grid-cols-12">
-        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2 bg-red-100">
+        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2 bg-red-100 mr-4">
           <Sidebar />
         </div>
         <div className="grid row-start-1 col-start-2 row-end-11 col-end-13">
-          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center bg-yellow-100">
+          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center bg-yellow-100 mb-4">
             <PageHeader />
           </div>
           <div className="grid row-start-2 col-start-1 row-end-11 col-end-11">
-            <div className="grid row-start-1 col-start-1 row-end-10 col-end-4 bg-green-100">
+            <div className="grid row-start-1 col-start-1 row-end-10 col-end-4 bg-green-100 mr-4">
               <DataAverager />
             </div>
             <div className="grid row-start-1 col-start-4 row-end-10 col-end-11">
               <div className="grid row-start-1 col-start-1 row-end-6 col-end-8 bg-blue-100">
                 <Graph />
               </div>
-              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8 bg-indigo-100">
+              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8 bg-indigo-100 mt-4">
                 <Map />
               </div>
             </div>
