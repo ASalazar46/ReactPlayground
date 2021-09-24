23b0aeb05ead62daa1f15778b5cb56f19e0f8da9
ASalazar46 <andysal737@gmail.com>
2021-09-23 20:20:26 -0700

    Centered text

--- dashboard-redux/src/components/PageHeader.js
+++ dashboard-redux/src/components/PageHeader.js
@@ -1,11 +1,16 @@
 import React from "react";
+import '../index.css';
 
 export class PageHeader extends React.Component {
     render() {
         return(
             <div>
-                <h1>Weatherbox Data</h1>
-                <h2>Last Updated: [Need current date and time here]</h2>
+                <div className="text-center">
+                    <span>Weatherbox Data</span>
+                </div>
+                <div className="text-center">
+                    <span>Last Updated: [Need current date and time here]</span>
+                </div>
             </div>
         );
     };
