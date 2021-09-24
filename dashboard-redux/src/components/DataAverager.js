0a48976f33a2ca064ed61771a9ed2ef3d60d21b3
ASalazar46 <andysal737@gmail.com>
2021-09-23 20:05:14 -0700

    Added placeholder boxes

--- dashboard-redux/src/components/DataAverager.js
+++ dashboard-redux/src/components/DataAverager.js
@@ -4,7 +4,18 @@ export class DataAverager extends React.Component {
     render() {
         return(
             <div>
-                <h1>Data gets averaged here</h1>
+                <div class="box-border h-32 w-32 p-4 border-4 m-0">
+                    1
+                </div>
+                <div class="box-border h-32 w-32 p-4 border-4 m-0">
+                    2
+                </div>
+                <div class="box-border h-32 w-32 p-4 border-4 m-0">
+                    3
+                </div>
+                <div class="box-border h-32 w-32 p-4 border-4 m-0">
+                    4
+                </div>  
             </div>
         );
     };
