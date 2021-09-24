36e089303673be56e81000a9b06dee5ebe3b5e7c
ASalazar46 <andysal737@gmail.com>
2021-09-23 20:21:13 -0700

    Modified layout and removed unneccessary comments

--- dashboard-redux/src/App.js
+++ dashboard-redux/src/App.js
@@ -9,22 +9,22 @@ function App() {
   return (
     <div className="App">
       <div className="grid grid-rows-10 grid-cols-12">
-        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2">
+        <div className="grid row-start-1 col-start-1 row-end-11 col-end-2 bg-red-100">
           <Sidebar />
         </div>
         <div className="grid row-start-1 col-start-2 row-end-11 col-end-13">
-          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center">
+          <div className="grid row-start-1 col-start-1 row-end-2 col-end-11 place-content-center bg-yellow-100">
             <PageHeader />
           </div>
           <div className="grid row-start-2 col-start-1 row-end-11 col-end-11">
-            <div className="grid row-start-1 col-start-1 row-end-10 col-end-4">
+            <div className="grid row-start-1 col-start-1 row-end-10 col-end-4 bg-green-100">
               <DataAverager />
             </div>
             <div className="grid row-start-1 col-start-4 row-end-10 col-end-11">
-              <div className="grid row-start-1 col-start-1 row-end-6 col-end-8">
+              <div className="grid row-start-1 col-start-1 row-end-6 col-end-8 bg-blue-100">
                 <Graph />
               </div>
-              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8">
+              <div className="grid row-start-6 col-start-1 row-end-10 col-end-8 bg-indigo-100">
                 <Map />
               </div>
             </div>
@@ -36,24 +36,3 @@ function App() {
 }
 
 export default App;
-/* 
-    <div className="App">
-      <div className="grid grid-rows-10 grid-cols-10">
-        <div className="row-start-1 col-start-1 row-end-10 col-end-2">
-          <Sidebar />
-        </div>
-        <div className="row-start-1 col-start-3 row-end-1 col-end-10">
-          <PageHeader />
-        </div>
-        <div className="row-start-2 col-start-3 row-end-10 col-end-5">
-          <DataAverager />
-        </div>
-        <div className="row-start-2 col-start-6 row-end-6 col-end-10">
-          <Graph />
-        </div>
-        <div className="row-start-7 col-start-6 row-end-10 col-end-10">
-          <Map />
-        </div>
-      </div>
-    </div>
-*/
