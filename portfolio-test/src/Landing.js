ef17bfb71841fbca2c4452125fee6bf284548438
ASalazar46 <andysal737@gmail.com>
2021-09-25 16:32:00 -0700

    Deleted and modified files

---
+++ portfolio-test/src/Landing.js
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar";
+
+function Landing() {
+  return (
+    <div className="App w-screen h-screen bg-pink-400">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default Landing;
