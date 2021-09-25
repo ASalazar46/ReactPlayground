f9b85e2745b98a848180e212d308f4a7160c06df
ASalazar46 <andysal737@gmail.com>
2021-09-24 22:52:46 -0700

    Added a simple navbar

---
+++ portfolio-test/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="w-screen h-12 min-w-min bg-red-100 grid grid-cols-10 
+      place-content-stretch text-center place-items-center">
+        <div className="col-start-1 col-span-2">
+          Portfolio Test
+        </div>
+        <div className="col-start-3 col-end-8">
+          <label>Contact Me:        </label>
+          <select>
+              <option className="select-text">(###) ### - ####</option>
+              <option className="select-text">sample.email@srvcr.ext</option>
+              <option className="select-text">linkedin.com/my-prof</option>
+          </select>
+        </div>
+        <button className="col-start-8 hover:bg-blue-100">
+          Essays
+        </button>
+        <button className="col-start-9 hover:bg-blue-100">
+          Works
+        </button>
+        <button className="col-start-10 hover:bg-blue-100">
+          Resume
+        </button>
+      </div>
+    );
+  }
+}
