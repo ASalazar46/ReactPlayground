4c8daa3b07754bf4ee88dbac14ff63ec216d7bae
ASalazar46 <andysal737@gmail.com>
2021-09-25 20:25:50 -0700

    Added responsive navbar text

--- portfolio-test/src/components/Navbar.js
+++ portfolio-test/src/components/Navbar.js
@@ -5,10 +5,10 @@ export class Navbar extends React.Component {
     return (
       <div className="h-12 min-w-full bg-gradient-to-r from-blue-400 to-indigo-400 
       grid grid-cols-10 place-content-stretch text-center place-items-center">
-        <div className="col-start-1 col-span-2">
+        <div className="col-start-1 col-span-2 font-mono text-sm md:text-lg lg:text-2xl">
           Portfolio Test
         </div>
-        <div className="col-start-3 col-end-8">
+        <div className="col-start-3 col-end-8 font-mono text-sm md:text-lg lg:text-2xl">
           <label>Contact Me:        </label>
           <select>
               <option className="select-text">(###) ### - ####</option>
@@ -16,13 +16,13 @@ export class Navbar extends React.Component {
               <option className="select-text">linkedin.com/my-prof</option>
           </select>
         </div>
-        <button className="col-start-8 hover:bg-blue-100">
+        <button className="col-start-8 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
           Essays
         </button>
-        <button className="col-start-9 hover:bg-blue-100">
+        <button className="col-start-9 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
           Works
         </button>
-        <button className="col-start-10 hover:bg-blue-100">
+        <button className="col-start-10 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
           Resume
         </button>
       </div>
