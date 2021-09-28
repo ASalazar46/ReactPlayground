434c2ab6f570e5561717c88054d311d04594e95d
ASalazar46 <andysal737@gmail.com>
2021-09-27 17:23:23 -0700

    Satisfied with component so far

--- portfolio-test/src/components/GreetMsg.js
+++ portfolio-test/src/components/GreetMsg.js
@@ -4,17 +4,19 @@ import placeImg from "../images/placeholder.jpg";
 export class GreetMsg extends React.Component {
   render() {
     return (
-      <div className="container w-10/12 h-3/4 mx-auto mt-16 mb-16 bg-gray-500 
-      rounded-xl grid grid-cols-2">
-        <div className="col-start-1 place-self-center">
-          <img className="p-4 mx-auto w-9/12 rounded-xl" src={placeImg} alt="placeholder" />
+      <div className="container w-11/12 h-auto mx-auto mt-16 mb-16 bg-gray-500 
+      rounded-xl grid grid-cols-3">
+        <div className="p-4 col-start-1 place-self-center">
+          <img className="w-11/12 rounded-xl" src={placeImg} alt="placeholder" />
         </div>
-        <div className="p-4 col-start-2 place-self-center">
-          <p className="font-mono 
-          sm:text-base md:text-lg lg:text-2xl">
-            Hi, my name is [name], and this is my portfolio!
-            <br />
-            <br />
+        <div className="font-mono p-4 col-start-2 col-span-2 place-self-center">
+          <p className="md:text-xl lg:text-4xl">
+            Hi, my name is [name here], and this is my WIP portfolio!
+          </p>
+          <br />
+          <br />
+          <br />
+          <p className="md:text-xl lg:text-4xl">
             I am a sentence that describes me. I am a second
             sentence that describes me. I am a third sentence
             that describes me, if I want to add it in here.
@@ -27,19 +29,24 @@ export class GreetMsg extends React.Component {
 
 /* 
   return (
-    <div className="w-screen h-screen bg-pink-400">
-      <Navbar />
-      <div className="grid grid-cols-2 h-5/6 place-items-center mt-4">
-        <div className="w-3/4">
-          <img src={placeImg} alt="placeholder" />
-        </div>
-        <div>
-          <p className="font-mono sm:text-xl md:text-4xl lg:text-6xl">Hi, my name is [Insert name here]!</p>
-          <br/>
-          <br/> 
-          <p className="font-mono sm:text-lg md:text-2xl lg:text-5xl">[Insert summary here]</p>
-        </div>
+    <div className="container w-auto h-auto mx-auto mt-16 mb-16 bg-gray-500 
+    rounded-xl grid grid-cols-3">
+      <div className="p-4 col-start-1 place-self-center">
+        <img className="w-9/12 rounded-xl" src={placeImg} alt="placeholder" />
       </div>
-    </div>
+      <div className="font-mono p-4 col-start-2 col-span-2 place-self-center">
+        <p className="md:text-xl lg:text-4xl">
+          Hi, my name is [name here], and this is my WIP portfolio!
+        </p>
+        <br />
+        <br />
+        <br />
+        <p className="sm:text-base md:text-md lg:text-xl">
+          I am a sentence that describes me. I am a second
+          sentence that describes me. I am a third sentence
+          that describes me, if I want to add it in here.
+        </p>
+      </div>
+    </div>  
   );
 */
