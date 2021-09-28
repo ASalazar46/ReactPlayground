9a1816e7a49d411727076223281f2633ed4afdf8
ASalazar46 <andysal737@gmail.com>
2021-09-28 16:41:07 -0700

    Added links to the template pages

--- portfolio-test/src/components/Navbar.js
+++ portfolio-test/src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export class Navbar extends React.Component {
   render() {
     return (
-      <div className="h-12 min-w-full bg-gradient-to-r from-blue-400 to-indigo-400 
-      grid grid-cols-10 place-content-stretch text-center place-items-center rounded-xl">
-        <div className="col-start-1 col-span-2 font-mono text-sm md:text-lg lg:text-2xl">
-          Portfolio Test
-        </div>
+      <div
+        className="h-12 min-w-full bg-gradient-to-r from-blue-400 to-indigo-400 
+      grid grid-cols-10 place-content-stretch text-center place-items-center rounded-xl"
+      >
+        <button className="col-start-1 col-span-2 font-mono text-sm md:text-lg lg:text-2xl">
+          <Link to="/">Portfolio Test</Link>
+        </button>
         <button className="col-start-8 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
-          Essays
+          <Link to="/essays">Essays</Link>
         </button>
         <button className="col-start-9 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
-          Works
+          <Link to="/works">Works </Link>
         </button>
         <button className="col-start-10 hover:bg-blue-100 font-mono text-sm md:text-lg lg:text-2xl">
-          Resume
+          <Link to="/resume">Resume </Link>
         </button>
       </div>
     );
--- portfolio-test/src/index.js
+++ portfolio-test/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Landing from './Landing';
+import React from "react";
+import ReactDOM from "react-dom";
+import { 
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import "./index.css";
+import Landing from "./pages/Landing";
+import EssayPage from "./pages/EssayPage";
+import WorksPage from "./pages/WorksPage";
+import ResumePage from "./pages/ResumePage";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Landing />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Route exact path="/" component={Landing} />
+    <Route path="/essays" component={EssayPage} />
+    <Route path="/works" component={WorksPage} />
+    <Route path="/resume" component={ResumePage} />
+  </Router>,
+  document.getElementById("root")
 );
-
